f288b2848205b10af9ffa1f2bfcdc78ee364a353
user1704 <95115321+user1704@users.noreply.github.com>
2022-05-18 21:18:29 +0800

    Update m.js

--- m.js
+++ m.js
@@ -9,8 +9,9 @@ class Frac {
       if (d == 0) {
         throw new Error("denominator cannot be zero!");
       }
-      this.n = n;
-      this.d = d;
+      let Gcd=gcd(n,d)
+      this.n = n/Gcd;
+      this.d = d/Gcd;
     }
     frac() {
       if(this.d<0){
@@ -24,45 +25,41 @@ class Frac {
     float() {
       return this.n / this.d
     }
-    reduce(){
-        let Gcd=gcd(this.n,this.d)
-        return new Frac(this.n/Gcd,this.d/Gcd)
-    }
     add(other) {
         if (other instanceof Frac) {
           let Gcd = gcd(this.d, other.d)
-          return new Frac(this.n * other.d + other.n * this.d,this.d * other.d ).reduce()
+          return new Frac(this.n * other.d + other.n * this.d,this.d * other.d )
         } else {
-          return new Frac(this.n + other * this.d, this.d).reduce()
+          return new Frac(this.n + other * this.d, this.d)
         }
       }
     sub(other) {
         if (other instanceof Frac) {
           let Gcd = gcd(this.d, other.d)
-          return new Frac(this.n * other.d - other.n * this.d,this.d * other.d ).reduce()
+          return new Frac(this.n * other.d - other.n * this.d,this.d * other.d )
         } else {
-          return new Frac(this.n - other * this.d, this.d).reduce()
+          return new Frac(this.n - other * this.d, this.d)
         }
       }
     mult(other) {
         if (other instanceof Frac) {
-          return new Frac(this.n * other.n, this.d * other.d).reduce()
+          return new Frac(this.n * other.n, this.d * other.d)
         } else {
-          return new Frac(this.n * other, this.d).reduce()
+          return new Frac(this.n * other, this.d)
         }
       }
     div(other) {
         if (other instanceof Frac) {
-          return new Frac(this.n * other.d, this.d * other.n).reduce()
+          return new Frac(this.n * other.d, this.d * other.n)
         } else {
-          return new Frac(this.n, this.d* other).reduce()
+          return new Frac(this.n, this.d* other)
         }
       }
     pow(exp) {
         if (exp < 0) {
-            return new Frac(this.d ** -exp, this.n ** -exp).reduce()
+            return new Frac(this.d ** -exp, this.n ** -exp)
         } else {
-            return new Frac(this.n ** exp, this.d ** exp).reduce()
+            return new Frac(this.n ** exp, this.d ** exp)
         }
     }
   }
